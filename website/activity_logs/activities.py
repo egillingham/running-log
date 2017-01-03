06adb8e275a85aac58df6ce6d8cfd5452658347f
Erin Gillingham <egillingham@findthebest.com>
2017-01-02 16:10:20 -0800

    Add activity: add all running log fields (and succesfully write)

--- website/activity_logs/activities.py
+++ website/activity_logs/activities.py
@@ -13,24 +13,12 @@ RUNNING_LOG_TABLE = 'running_log'
 
 class Activities(object):
 
-    ACTIVITY_FIELDS = ['activity', 'date', 'sick', 'traveling', 'latitude', 'longitude']
-    ACTIVITY_FORM_FIELDS = OrderedDict({'date': {'title': 'Date Time', 'type': 'datetime-local', 'order': 1},
-                                        'sick': {'title': 'Currently Sick?', 'type': 'checkbox', 'value': True,
-                                                 'order': 2},
-                                        'traveling': {'title': 'Currently Traveling?', 'type': 'checkbox',
-                                                      'value': True, 'order': 3},
-                                        'latitude': {'title': 'Latitude', 'type': 'number', 'order': 4},
-                                        'longitude': {'title': 'Longitude', 'type': 'number', 'order': 5}
-                            })
-    RUNNING_FIELDS = ['date', 'miles', 'minutes', 'feeling', 'extras', 'description', 'terrain', 'course',
-                      'course_description', 'miles_approx', 'minutes_approx', 'stretch', 'workout']
-    RUNNING_FORM_FIELDS = OrderedDict({})
-
     def __init__(self):
         self.conn = connect(host=mysql_creds['host'], port=mysql_creds['port'], user=mysql_creds['user'],
                             passwd=mysql_creds['password'], db='logs', cursorclass=cursors.DictCursor)
         self.activity_lookup = self.get_activity_list()
         self.activity_list = list(self.activity_lookup.keys())
+        self.fields_lists = {}
 
     def get_key_value_pair(self, field):
         query = Query(self.conn, KEY_VALUE_TABLE)
@@ -45,26 +33,45 @@ class Activities(object):
 
         return activity_lookup
 
+    def get_fields(self, table):
+        query = Query(self.conn, FIELD_INFO_TABLE)
+        field_info_fields = ['field']
+        fields = query.select(field_info_fields, where="field_table = '{}'".format(table))
+        # cache fields for later
+        fields = [row.get('field') for row in fields]
+        self.fields_lists[table] = fields
+        return fields
+
     def get_form_fields(self, table, seen_list=None):
-        where = "field_table = '{}'".format(table)
         query = Query(self.conn, FIELD_INFO_TABLE)
         field_info_fields = ['field, display_name, form_type, form_order, form_value, placeholder', 'normalized']
-        fields = query.select(field_info_fields, where=where)
+        fields = query.select(field_info_fields, where="field_table = '{}'".format(table))
         if not seen_list:
+            # if seen list is not passed in, start a new one
             seen_list = []
         form_fields = []
+        insert_fields = []
         for field in fields:
+            # add field to insert fields list, to be set to the class to be used later if needed
+            insert_fields.append(field.get('field'))
+            # for the rest: ignore fields that have been seen or don't have a form order (aren't in form yet)
             if field in seen_list or not field.get('form_order'):
                 continue
 
             seen_list.append(field)
             if field.get('normalized'):
+                # if field is normalized, get the list of values from key values table
                 values = self.get_key_value_pair(field.get('field'))
                 values = [row.get('field_value') for row in values]
                 field['form_value'] = values
+            # append field to final form fields dict
             form_fields.append(field)
 
+        # set insert list to class
+        self.fields_lists[table] = insert_fields
+        # order fields into desired order
         form_fields = sorted(form_fields, key=lambda k: k['form_order'])
+        # return seen list so it can be used by another form creation
         return form_fields, seen_list
 
     def get_activity_fields(self):
@@ -78,7 +85,8 @@ class Activities(object):
 
     def add_activity(self, activity_info):
         # write to activity log
-        activity_log_data = {k: v for (k, v) in activity_info.items() if k in self.ACTIVITY_FIELDS}
+        activity_fields = self.fields_lists.get(ACTIVITY_LOG_TABLE, self.get_fields(ACTIVITY_LOG_TABLE))
+        activity_log_data = {k: v for (k, v) in activity_info.items() if k in activity_fields}
         # if no activity data, return
         if not activity_log_data or not activity_log_data.get('date') or not activity_log_data.get('activity'):
             return False
@@ -86,5 +94,20 @@ class Activities(object):
         activity_log_data['activity'] = activity
         query = Query(self.conn, ACTIVITY_LOG_TABLE)
         query.insert_update([activity_log_data])
+
+        if activity == 1:
+            # write to running log
+            run_fields = self.fields_lists.get(RUNNING_LOG_TABLE, self.get_fields(RUNNING_LOG_TABLE))
+            run_log_data = {k: v for (k, v) in activity_info.items() if k in run_fields}
+            # if no activity data, return
+            if not run_log_data or not run_log_data.get('date'):
+                return False
+            # change terrain back to keys
+            if run_log_data.get('terrain'):
+                terrain = self.get_key_value_pair('terrain')
+                terrain_lookup = {row.get('field_value'): int(row.get('field_key')) for row in terrain}
+                run_log_data['terrain'] = terrain_lookup[run_log_data['terrain']]
+            query = Query(self.conn, RUNNING_LOG_TABLE)
+            query.insert_update([run_log_data])
 
         return True
