0ca0e60647de9b068e6aeb5fe1bc751cec474c8b
Erin Gillingham <egillingham@findthebest.com>
2017-02-16 23:19:02 -0800

    Login Popup: loads html from endpoint and removes html on exit

--- website/static/login.js
+++ website/static/login.js
@@ -1,13 +1,44 @@
 
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() {
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );
+        anHttpRequest.send( null );
+    }
+}
+
 function render_login(class_to_render) {
     // Get the modal
-    var modal = document.getElementById(class_to_render);
-    modal.style.display = "block";
+    var popUpDiv = document.createElement('div');
+    popUpDiv.setAttribute("id", "login-popup");
+    popUpDiv.setAttribute("class", "popup");
+    popUpDiv.setAttribute("style", "display: block");
+
+    var client = new HttpClient;
+    client.get('/login', function(response) {
+        var content = document.createElement('div');
+        content.innerHTML = response;
+
+        var popUpContentDiv = document.createElement('div');
+        popUpContentDiv.setAttribute("class", "popup-content");
+
+        var content_html = content.getElementsByClassName("page")[0];
+        popUpContentDiv.appendChild(content_html);
+        popUpDiv.appendChild(popUpContentDiv);
+        document.body.appendChild(popUpDiv);
+    });
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+        if (event.target.id == "login-popup") {
+            event.target.outerHTML = "";
+            delete event.target;
         }
     }
+
 }
