a4db27bfa3cad809685abcc8d4c6ba9b51d18ad3
Erin Gillingham <egillingham@findthebest.com>
2017-03-12 23:30:11 -0700

    Charting: add tool tip to stacked bar chart

--- website/static/stacked_bar.js
+++ website/static/stacked_bar.js
@@ -12,8 +12,12 @@ function stackedBar(data, base_class, yAxisTitle, tot_height, tot_width) {
     // color
     var color = d3.scaleOrdinal(["#adc698", "#61988e", "#592d51", "#eabda8"]);
 
+    // date format
+    var dateFormat = d3.timeFormat("%B %e, %Y")
+
     // char set-up
-    var chart = d3.select(base_class)
+    var chart_base = d3.select(base_class);
+    var chart = chart_base.append("div").attr("class", "chart").style("position", "relative");
     var legend = chart.append("div").attr("class", "legend");
     var svg = chart.append('svg').attr('height', tot_height).attr('width', tot_width);
     var width = svg.attr("width") - margin.left - margin.right,
@@ -38,6 +42,7 @@ function stackedBar(data, base_class, yAxisTitle, tot_height, tot_width) {
 
     var x = d3.scaleTime()
       .range([0, width])
+      // TODO: make date buffers dynamic to range of data
       .domain([d3.min(data, function(d) {date = new Date(Date.parse(d.date)); return date.setDate(date.getDate() - 1); }),
                d3.max(data, function(d) {date = new Date(Date.parse(d.date)); return date.setDate(date.getDate() + 1); })
               ]);
@@ -63,10 +68,31 @@ function stackedBar(data, base_class, yAxisTitle, tot_height, tot_width) {
             // here, we set the top-left of this bar segment to the
             // larger value of the pair
             return y(d[1]);
-          }).attr('height', function(d) {
+          })
+          .attr('height', function(d) {
             // since we are drawing our bar from the top downwards,
             // the length of the bar is the distance between our points
             return y(d[0]) - y(d[1]);
+          })
+          .on("mouseover", function() {
+            tooltip.style("display", "inline-block");
+            })
+          .on("mouseout", function() { tooltip.style("display", "none"); })
+          .on("mousemove", function(d) {
+            var xPosition = d3.mouse(this)[0] - 15;
+            var yPosition = y(d[1]);
+            var cur_date = new Date(d.data.date);
+            var tool_text = '';
+            for (var k in data_keys) {
+                if (d.data[data_keys[k]] != null) {
+                    t = data_keys[k] + ": " + d.data[data_keys[k]] + " " + yAxisTitle + "<br>";
+                    tool_text += t;
+                }
+            }
+            tooltip.style("left", xPosition + 'px');
+            tooltip.style("top", yPosition + 'px');
+            tooltip.select("div.tooltip-title").text(dateFormat(cur_date));
+            tooltip.select("div.tooltip-text").html(tool_text);
           });
 
     // AXIS
@@ -103,6 +129,24 @@ function stackedBar(data, base_class, yAxisTitle, tot_height, tot_width) {
     legend_items.append('div')
       .attr('class', 'legend-txt')
       .text(function(d) { return d.key; });
+
+    // Prep the tooltip bits, initial display is hidden
+    var tooltip = chart.append("div")
+      .attr("class", "tooltip");
+
+    tooltip.append("div")
+      .attr("class", "tooltip-title")
+      .attr("x", 15)
+      .attr("dy", "1.2em")
+      .attr("width", 50)
+      .attr("height", 50)
+
+    tooltip.append("div")
+      .attr("class", "tooltip-text")
+      .attr("x", 15)
+      .attr("dy", "1.2em")
+      .attr("width", 50)
+      .attr("height", 50)
 
 }
 
