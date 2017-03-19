260ab7d77a19ec3b6b2ec08976d7155d90fdf27a
Erin Gillingham <egillingham@findthebest.com>
2017-03-19 15:04:17 -0700

    Footer: add CSS disclosure/appology

--- website/static/popup.js
+++ website/static/popup.js
@@ -42,3 +42,31 @@ function render_popup(popup_id, endpoint) {
     }
 
 }
+
+function render_popup_content(popup_id, content_text) {
+    // Get the modal
+    var popUpDiv = document.createElement('div');
+    popUpDiv.setAttribute("id", popup_id);
+    popUpDiv.setAttribute("class", "popup");
+    popUpDiv.setAttribute("style", "display: block");
+
+    var content = document.createElement('div');
+    content.innerHTML = content_text;
+
+    var popUpContentDiv = document.createElement('div');
+    popUpContentDiv.setAttribute("class", "popup-content");
+
+    var content_html = content.getElementsByClassName("page")[0];
+    popUpContentDiv.appendChild(content);
+    popUpDiv.appendChild(popUpContentDiv);
+    document.body.appendChild(popUpDiv);
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target.id == popup_id) {
+            event.target.outerHTML = "";
+            delete event.target;
+        }
+    }
+
+}
