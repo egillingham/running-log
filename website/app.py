4e240c2b842f1216e6896ee51bc6d46e37874335
Erin Gillingham <e.gillingham9@gmail.com>
2020-04-12 18:37:28 +0100

    Use Aurora; get secrests from SM; add icon

--- website/app.py
+++ website/app.py
@@ -1,4 +1,7 @@
+import logging
 import os
+import json
+from logstash_formatter import LogstashFormatterV1
 from flask import Flask, url_for, render_template
 from flask_restful import Api
 # from flask_sitemap_domain import Sitemap
@@ -47,15 +50,31 @@ def images(name):
 APP.config['SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS'] = True
 
 
+def create_logger():
+    log = logging.getLogger('running-log')
+    log.setLevel(logging.DEBUG)
+
+    fmt = {'extra': {'type': 'running-log'}}
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(LogstashFormatterV1(fmt=json.dumps(fmt)))
+    log.addHandler(ch)
+    return log
+
+
 if __name__ == '__main__':
-    print("In __main__, running host...")
+    LOGGER = create_logger()
+    LOGGER.info("In __main__, running host...")
     APP.secret_key = 'secret_test'
     APP.run(host='0.0.0.0', port=5005, debug=True)
 
 elif __name__ == 'app':
     # when we run using gunicorn
-    print("WSGI setup...")
-    APP.secret_key = os.environ.get('FLASK_SECRET')
+    LOGGER = create_logger()
+    LOGGER.info("WSGI setup...")
+    flask_env = json.loads(os.environ.get("FLASK_SECRET", "{}"))
+    APP.secret_key = flask_env.get("secret", "test")
     # might need this for https
     # https://flask.palletsprojects.com/en/1.1.x/deploying/wsgi-standalone/#deploying-proxy-setups
     APP.wsgi_app = ProxyFix(APP.wsgi_app, x_proto=1, x_host=1)
